707a3f1cf22676796863a439d03e59a01e4c8968
Sriya Kantipudi <sriyakantipudi@berkeley.edu>
2023-10-02 00:00:11 -0700

    finished project and recorded demo

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Image, Text, View, TouchableOpacity } from "react-native";
-
 import { styles } from "../constants/Styles";
 import { nameToPic } from "../constants/Constants";
 import { useEffect } from "react";
@@ -8,14 +7,39 @@ import { shuffle } from "../utils/ArrayUtils";
 
 //this file contains the GameScreen, which is where the game is played.
 
+//array of names
 const names = Object.keys(nameToPic);
 
 export default function GameScreen() {
   // TODO: Declare and initialize state variables here, using "useState".
 
-  // State for the timer is handled for you.
-  const [timeLeft, setTimeLeft] = useState(5000);
+  //score is set to 0 for beginning of game 
+  //setScore will update score
+  const [currentScore, setCurrentScore] = useState(0);
 
+  //total questions to keep track of # of questions asked
+  //setTotalQuestions will update score
+  const [totalQuestions, setTotalQuestions] = useState(0);
+
+  //intializes nameOptions to be an empty array 
+  //set NameOptions will update nameOptions
+  const [nameOptions, setNameOptions] = useState([]);
+
+  const [correctName, setCorrectName] = useState("");
+  //initializes memberName with initial value of empty string ""
+  //setMemberName will update memberName 
+
+  const [correctPicture, setCorrectPicture] = useState("");
+  //initializes correctPicture with initial value of empty string ""
+  //setCorrectPicture will update correctPicture
+
+  // State for the timer is handled for you.
+  // 5 second timer 
+  const [timeLeft, setTimeLeft] = useState(5000); //5000 milliseconds aka 5 seconds
+  //setTimeLeft updates the timeLeft variable with new value 
+  //component rerenders with the updated state
+
+  //Functional component called countDown 
   // Called by the timer every 10 seconds
   const countDown = () => {
     if (timeLeft > 0) {
@@ -24,6 +48,7 @@ export default function GameScreen() {
     } else {
       // Time has expired
       // TODO: update appropriate state variables
+      setTimeLeft(5000);
     }
   };
 
@@ -48,13 +73,20 @@ export default function GameScreen() {
 
     // TODO: Update state here.
 
+    setCorrectName(correctName);
+    setCorrectPicture(correctImage);
+    setNameOptions(nameOptions);
     setTimeLeft(5000);
   };
 
   // Called when user taps a name option.
   // TODO: Update correct # and total # state values.
-  const selectedNameChoice = (index) => {};
-
+  const selectedNameChoice = (index) => {
+  if (nameOptions[index] === correctName) {
+    setCurrentScore(currentScore + 1);
+  }
+  setTotalQuestions(totalQuestions + 1)
+};
   // Call the countDown() method every 10 milliseconds.
   useEffect(() => {
     const timer = setInterval(() => countDown(), 10);
@@ -71,6 +103,11 @@ export default function GameScreen() {
     },
     [
       /* TODO: Your State Variable Goes Here */
+      totalQuestions
+      //what this useEffect does is that it it's triggered whenever this variable changes 
+      //and when totalQuestions changes, is when we want to move on to the next round
+
+
     ]
   );
 
@@ -78,6 +115,8 @@ export default function GameScreen() {
   const nameButtons = [];
   for (let i = 0; i < 4; i++) {
     const j = i;
+
+    //adding to nameButtons
     nameButtons.push(
       // A button is just a Text component wrapped in a TouchableOpacity component.
       <TouchableOpacity
@@ -87,20 +126,33 @@ export default function GameScreen() {
       >
         <Text style={styles.buttonText}>
           {/* TODO: Use something from state here. */}
+          {nameOptions[i]}
+
         </Text>
       </TouchableOpacity>
     );
   }
 
   const timeRemainingStr = (timeLeft / 1000).toFixed(2);
-
+//{nameButtons.map((item)=>{return item
   // Style & return the view.
-  return (
-    <View>
-      {/* TODO: Build out your UI using Text and Image components. */}
+  {/* TODO: Build out your UI using Text and Image components. */}
       {/* Hint: What does the nameButtons list above hold? 
           What types of objects is this list storing?
           Try to get a sense of what's going on in the for loop above. */}
+  return (
+    <View style = {styles.container}>
+      <View>
+      <Text style={styles.scoreText}>Current Score: {currentScore}/{totalQuestions}</Text>
+      <Text style={styles.timerText}>Time Remaining: {timeLeft/1000} </Text>
+    </View>
+    <Image style = {styles.image} source = {correctPicture}/>
+        <View>
+          {nameButtons[0]}
+          {nameButtons[1]}
+          {nameButtons[2]}
+          {nameButtons[3]}
+        </View>
     </View>
   );
 }
